412e8be4e780cb6db9d2f67daaabf7d621589f58
namoamitabhapureland <huangsir945@gmail.com>
2026-09-01 13:50:23 +0800

    Update config_en.js

--- config_en.js
+++ config_en.js
@@ -1,10 +1,12 @@
 // config_en.js - Amitabha Pure Land System (English) Festival & Dew Quotes
 
 const festivalMap = {
-    "01-05": "Amitabha Buddha's Birthday", "12-25": "Amitabha Buddha's Birthday",
-    "04-06": "Avalokitesvara's Birthday", "08-01": "Avalokitesvara's Enlightenment", "10-28": "Avalokitesvara's Renunciation",
-    "09-10": "Ksitigarbha Bodhisattva's Birthday", "05-24": "Shakyamuni Buddha's Birthday",
-    "03-26": "Shakyamuni Buddha's Renunciation", "04-02": "Shakyamuni Buddha's Nirvana",
+    // 帶有連結的英文公告/節日
+    "07-29": { text: "[System] Bodhisattvas Samantabhadra & Manjusri Enshrined", url: "48vows.html" },
+    "09-01": { text: "Your monthly feedback", url: "lunar_en.html" },
+    
+ 
+
     "01-19": "Lunar 1st", "02-02": "Lunar 15th", "02-17": "Lunar 1st", "03-03": "Lunar 15th", "03-19": "Lunar 1st",
     "04-17": "Lunar 1st", "05-01": "Lunar 15th", "05-17": "Lunar 1st", "05-31": "Lunar 15th", "06-15": "Lunar 1st",
     "06-29": "Lunar 15th", "07-14": "Lunar 1st", "07-28": "Lunar 15th", "08-13": "Lunar 1st", "08-27": "Lunar 15th",
